0e17f87c3531b3184c2ef9d40a694c61ecc6e9aa
Zoaib <thisiszoaib@gmail.com>
2024-11-26 10:46:13 +0500

    starting point for the course

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,3 @@
 import { Routes } from '@angular/router';
-import { ProductsListComponent } from './pages/products-list/products-list.component';
-import { CartComponent } from './pages/cart/cart.component';
 
-export const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    component: ProductsListComponent,
-  },
-  {
-    path: 'cart',
-    component: CartComponent,
-  },
-];
+export const routes: Routes = [];
