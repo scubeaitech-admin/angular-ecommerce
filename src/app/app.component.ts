e13e820896fba244ce5bf542cd4d3e94ae929150
Zoaib <thisiszoaib@gmail.com>
2024-11-26 10:04:41 +0500

    Simple ecommerce app complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <app-header />
     <router-outlet />
   `,
   styles: [],
 })
-export class AppComponent {
-  title = 'angular-ecommerce';
-}
+export class AppComponent {}
